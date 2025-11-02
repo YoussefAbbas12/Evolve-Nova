1c20f09d11b89073a0a6bfb9819d1dc927042897
Mahmoud Nagy <nagymedo25@gmail.com>
2025-11-02 19:16:00 +0200

    Final Pro

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5000,
-    strictPort: true,
-    hmr: {
-      host: process.env.REPLIT_DEV_DOMAIN,
-      clientPort: 443,
-      protocol: 'wss'
-    }
+    proxy: {
+      '/api': {
+        target: 'https://evolve-nova-back.vercel.app/',
+        changeOrigin: true,
+      },
+    },
   },
-  preview: {
-    host: '0.0.0.0',
-    port: 5000,
-    strictPort: true
-  }
 })
